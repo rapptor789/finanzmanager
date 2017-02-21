e1c1c4beef91ec02f128a2b91a05ffeb9f1f1cf3
Michael Hallensleben <m.hallensleben@ibhsoft.de>
2017-02-21 13:23:21 +0100

    button example

--- app.js
+++ app.js
@@ -23,6 +23,31 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+var salesData = {
+    "Kleinkredit aufnehmen": {
+        units: 200,
+        total: "$6,000"
+    },
+    "Von Tagesgeldkonto Geld tranferieren": {
+        units: 100,
+        total: "$3,000"
+    },
+    "Alles OK nichts tun": {
+        units: 300,
+        total: "$9,000"
+    }
+};
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.choice(session, "Dein Girokonto ist im Minus... Wir haben folgende Optionen für Dich ermittelt:", salesData, {listStyle: builder.ListStyle['button']}); 
+    },
+    function (session, results) {
+        if (results.response) {
+            var region = salesData[results.response.entity];
+            session.send("We sold %(units)d units for a total of %(total)s.", region); 
+        } else {
+            session.send("ok");
+        }
+    }
+]);
