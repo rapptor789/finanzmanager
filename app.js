c4492c35714a33ef3c7affed34854666dadf3d71
Michael Hallensleben <m.hallensleben@ibhsoft.de>
2017-02-22 10:34:09 +0100

    fix typo

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
-    appPassword: sprocess.env.MICROSOFT_APP_PASSWORD
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
